dee3b55da61f0e667dd126e18fc14b1eaeb07438
Sanka Darshana <sankadarshana@gmail.com>
2022-03-14 09:14:34 +0530

    setting up pilot project

--- saito-core/src/core/storage.rs
+++ saito-core/src/core/storage.rs
@@ -1,7 +1,17 @@
+use std::io::Error;
+
 use crate::common::handle_io::HandleIo;
 
 pub struct Storage {
     io_handler: dyn HandleIo,
 }
 
-impl Storage {}
+impl Storage {
+    pub fn save_data(&mut self, key: String, value: Vec<u8>) -> Result<(), Error> {
+        self.io_handler.write_value(key, value)
+    }
+
+    pub fn load_value(&self, key: String) -> Result<Vec<u8>, Error> {
+        self.io_handler.read_value(key)
+    }
+}
