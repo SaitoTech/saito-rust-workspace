bc5eaab6bf665500636301a7204b2a18af78ec5e
Sanka Darshana <sankadarshana@gmail.com>
2022-04-29 12:59:14 +0530

    Wind/Unwind chain/Reorganization Functionality #14
    test fixes

--- saito-rust/src/test/test_manager.rs
+++ saito-rust/src/test/test_manager.rs
@@ -3,10 +3,16 @@
 // help make tests more succinct.
 //
 
-use crate::test::test_io_handler::TestIOHandler;
+use std::sync::Arc;
+use std::time::{SystemTime, UNIX_EPOCH};
+use std::{thread::sleep, time::Duration};
+
 use ahash::AHashMap;
 use log::{debug, info};
 use rayon::prelude::*;
+use tokio::sync::mpsc::Sender;
+use tokio::sync::RwLock;
+
 use saito_core::common::defs::{SaitoHash, SaitoPrivateKey, SaitoPublicKey, SaitoUTXOSetKey};
 use saito_core::common::handle_io::HandleIo;
 use saito_core::core::data::block::{Block, BlockType};
@@ -19,11 +25,8 @@ use saito_core::core::data::peer_collection::PeerCollection;
 use saito_core::core::data::transaction::{Transaction, TransactionType};
 use saito_core::core::data::wallet::Wallet;
 use saito_core::core::miner_controller::MinerEvent;
-use std::sync::Arc;
-use std::time::{SystemTime, UNIX_EPOCH};
-use std::{thread::sleep, time::Duration};
-use tokio::sync::mpsc::Sender;
-use tokio::sync::RwLock;
+
+use crate::test::test_io_handler::TestIOHandler;
 
 pub fn create_timestamp() -> u64 {
     SystemTime::now()
@@ -70,6 +73,7 @@ impl TestManager {
     }
     pub async fn clear_data_folder() {
         tokio::fs::remove_dir_all("data").await.unwrap();
+        tokio::fs::create_dir_all("data").await.unwrap();
     }
     //
     // add block at end of longest chain
--- saito-rust/src/test/test_setup.rs
+++ saito-rust/src/test/test_setup.rs
@@ -3,4 +3,5 @@
 fn init_tests() {
     // initialize a logger only for tests (to capture info and trace logs based on RUST_LOG)
     pretty_env_logger::init();
+    std::fs::create_dir_all("data");
 }
