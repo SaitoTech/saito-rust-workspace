d0b573a0b34548c6f3062eda42cd0ce71c5ba563
Sanka Darshana <sankadarshana@gmail.com>
2022-04-29 12:21:00 +0530

    Wind/Unwind chain/Reorganization Functionality #14
    Implementing network servers and fetching functionality for block fetch.

--- saito-core/src/core/data/blockchain.rs
+++ saito-core/src/core/data/blockchain.rs
@@ -763,8 +763,10 @@ impl Blockchain {
             info!(
                 "{} - {:?}",
                 current_id,
-                self.blockring
-                    .get_longest_chain_block_hash_by_block_id(current_id)
+                hex::encode(
+                    self.blockring
+                        .get_longest_chain_block_hash_by_block_id(current_id)
+                )
             );
             current_id -= 1;
         }
