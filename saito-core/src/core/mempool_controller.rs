486f191f66ffc064eb4c0054c0ed50f26a3accc8
Sanka Darshana <sankadarshana@gmail.com>
2022-05-09 16:37:45 +0530

    Block syncing between 2 nodes #47
    implementation for block sync at peer connection

--- saito-core/src/core/mempool_controller.rs
+++ saito-core/src/core/mempool_controller.rs
@@ -51,7 +51,7 @@ impl MempoolController {
         wallet: Arc<RwLock<Wallet>>,
         blockchain: Arc<RwLock<Blockchain>>,
     ) {
-        debug!("generating mock transactions");
+        trace!("generating mock transactions");
 
         let mempool_lock_clone = mempool.clone();
         let wallet_lock_clone = wallet.clone();
@@ -127,7 +127,7 @@ impl MempoolController {
                     .await;
             }
         }
-        debug!("generated transaction count: {:?}", txs_to_generate);
+        trace!("generated transaction count: {:?}", txs_to_generate);
     }
 }
 
