a30be640ab02a28013bd01bedcfdfe513832cfa4
Sanka Darshana <sankadarshana@gmail.com>
2022-09-05 12:37:14 +0530

    Perf Testing on Rust node #178

--- saito-core/benches/benches.rs
+++ saito-core/benches/benches.rs
@@ -3,6 +3,6 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 mod benchmarks;
 
 criterion_main! {
-    // benchmarks::hashing::hashing_group,
+    benchmarks::hashing::hashing_group,
     benchmarks::serialize_tx::serializing_tx_group
 }
