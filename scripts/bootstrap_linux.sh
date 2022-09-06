333e195bddde6a7c325e4d5810246d38d60473fa
Sanka Darshana <sankadarshana@gmail.com>
2022-09-06 10:25:16 +0530

    Perf Testing on Rust node #178

--- scripts/bootstrap_linux.sh
+++ scripts/bootstrap_linux.sh
@@ -3,9 +3,8 @@
 # Script to setup the basic requirements for running a saito rust node
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y || exit
 sudo apt update
-sudo apt install git build-essential libssl-dev pkg-config || exit
-git clone https://github.com/SaitoTech/saito-rust-workspace.git || exit
-cd saito-rust-workspace || exit
+sudo apt install build-essential libssl-dev pkg-config || exit
+bash
 cargo build || exit
 
 # setup the saito-rust/saito.config.json file from the template and run `cargo run`
