6be3c2d72385c4a612fc6f997869182dbb252378
Sanka Darshana <sankadarshana@gmail.com>
2022-09-30 10:40:27 +0530

    Perf Testing on Rust node #181

--- saito-spammer-new/src/saito/transaction_generator.rs
+++ saito-spammer-new/src/saito/transaction_generator.rs
@@ -247,8 +247,7 @@ impl TransactionGenerator {
                     log_write_lock_request!("wallet");
                     let mut wallet = wallet.write().await;
                     log_write_lock_receive!("wallet");
-                    let create_count = min(100000, required_count);
-                    // let mut txs = Vec::with_capacity(create_count as usize);
+                    let create_count = min(1000, required_count);
                     for _i in 0..create_count {
                         let mut transaction = Transaction::create(&mut wallet, public_key, 1, 0);
                         transaction.message = generate_random_bytes(tx_size as u64);
@@ -258,16 +257,11 @@ impl TransactionGenerator {
                         transaction.add_hop(&wallet, &public_key);
 
                         sender.send(transaction).await.unwrap();
-                        // txs.push(transaction);
 
                         required_count -= 1;
                     }
-                    // for tx in txs {
-                    //     sender.send(tx).await.unwrap();
-                    // }
                 }
                 info!("test transactions to be created : {:?}", required_count);
-                // tokio::time::sleep(Duration::from_millis(10)).await;
                 if required_count == 0 {
                     break;
                 }
