6211bd21066a113c942b8078e610aa23d4c41670
Sanka Darshana <sankadarshana@gmail.com>
2022-10-14 11:02:37 +0530

    Perf Testing on Rust node #181

--- saito-rust/src/main.rs
+++ saito-rust/src/main.rs
@@ -385,9 +385,9 @@ async fn run_verification_threads(
         };
         let (_interface_sender_to_verification, interface_receiver_for_verification) =
             tokio::sync::mpsc::channel::<NetworkEvent>(1);
-        let thread_handle = run_thread(
+        let thread_handle = run_verification_thread(
             Box::new(verification_thread),
-            interface_receiver_for_verification,
+            // interface_receiver_for_verification,
             receiver,
             stat_timer_in_ms,
             thread_sleep_time_in_ms,
