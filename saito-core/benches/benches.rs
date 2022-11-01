777213659249689c588f84c294ff472b41728892
Sanka Darshana <sankadarshana@gmail.com>
2022-11-01 06:57:39 +0530

    random very small blocks are being created when it should not #224

--- saito-core/benches/benches.rs
+++ saito-core/benches/benches.rs
@@ -3,8 +3,8 @@ use criterion::criterion_main;
 mod benchmarks;
 
 criterion_main! {
-    // benchmarks::hashing::hashing_group,
-    // benchmarks::serialize_tx::serializing_tx_group,
-    // benchmarks::serialize_block::serializing_block_group,
+    benchmarks::hashing::hashing_group,
+    benchmarks::serialize_tx::serializing_tx_group,
+    benchmarks::serialize_block::serializing_block_group,
     benchmarks::misc::misc_group,
 }
