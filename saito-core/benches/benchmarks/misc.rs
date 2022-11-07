d82b9620f2d8d8fd5211b3564c7e077fac2ff692
Sanka Darshana <sankadarshana@gmail.com>
2022-11-07 10:40:45 +0530

    Getting numbers related to block sizes#214

--- saito-core/benches/benchmarks/misc.rs
+++ saito-core/benches/benchmarks/misc.rs
@@ -1,5 +1,7 @@
 use criterion::{criterion_group, Criterion};
 
+use saito_core::core::data::crypto::generate_random_bytes;
+
 fn extend() {
     let buffer = [0; 33];
     let buffer2 = [0; 32];
@@ -33,11 +35,25 @@ fn join() {
     assert!(buffer.len() > 0);
 }
 
+fn gen_random_bytes(len: u64) {
+    let buf = generate_random_bytes(len);
+    assert_eq!(buf.len(), len as usize);
+}
+
 pub fn misc(c: &mut Criterion) {
     c.bench_function("extend", |b| b.iter(|| extend()));
     c.bench_function("append", |b| b.iter(|| append()));
     c.bench_function("concat", |b| b.iter(|| concat()));
     c.bench_function("join", |b| b.iter(|| join()));
+    c.bench_function("gen_random_bytes_1", |b| b.iter(|| gen_random_bytes(1)));
+    c.bench_function("gen_random_bytes_10", |b| b.iter(|| gen_random_bytes(10)));
+    c.bench_function("gen_random_bytes_100", |b| b.iter(|| gen_random_bytes(100)));
+    c.bench_function("gen_random_bytes_1000", |b| {
+        b.iter(|| gen_random_bytes(1000))
+    });
+    c.bench_function("gen_random_bytes_1000000", |b| {
+        b.iter(|| gen_random_bytes(1000000))
+    });
 }
 
 criterion_group!(misc_group, misc);
