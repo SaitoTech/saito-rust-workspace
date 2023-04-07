dc59e643e143685a8e1b53859766af3f5801f2b4
Sanka Darshana <sankadarshana@gmail.com>
2023-04-07 09:29:51 +0530

    Integrate callbacks and other functionalities required for SLR #282

--- scripts/bootstrap_linux.sh
+++ scripts/bootstrap_linux.sh
@@ -3,9 +3,10 @@
 # Script to setup the basic requirements for running a saito rust node
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y || exit
 sudo apt update
-sudo apt install build-essential libssl-dev pkg-config || exit
+sudo apt install build-essential libssl-dev pkg-config nodejs npm clang gcc-multilib python-is-python3 || exit
 bash
 cargo install flamegraph
+cargo install wasm-pack
 cargo build || exit
 
 # setup the saito-rust/config.json file from the template and run `cargo run`
