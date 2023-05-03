08dbdb77c78679fb196a7bf0064e7a65a0ac9394
Sanka Darshana <sankadarshana@gmail.com>
2023-05-03 15:17:24 +0530

    Publishing saito-wasm and saito-js into npmjs #323

---
+++ scripts/boostrap_for_ci.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Script to setup the basic requirements for running a saito rust node
+#curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+sudo apt update
+sudo NEEDRESTART_MODE=a apt install -y build-essential libssl-dev pkg-config clang gcc-multilib python-is-python3
+tset
+cargo install flamegraph
+cargo install wasm-pack
+
+# setup the saito-rust/config.json file from the template and run `cargo run`
