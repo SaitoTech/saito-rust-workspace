bbe497e4bb2ddc2aac09ddd822bdd41908ca9c5d
davik20 <onyejivic@gmail.com>
2023-09-14 11:14:32 +0100

    update

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-VERSION=$(cat ../VERSION | tr -d '\n')
+VERSION=$(cat ./VERSION | tr -d '\n')
+
+echo "$VERSION"
 
 update_cargo() {
     local cargo_file="$1"
     if [[ -f $cargo_file ]]; then
         OS=$(uname)
-        if [[ "$OS" == "Darwin" ]]; then 
+        if [[ "$OS" == "Darwin" ]]; then
             sed -i "" "s/^version = .*/version = \"$VERSION\"/" $cargo_file
+        else
+            sed -i "s/^version = .*/version = \"$VERSION\"/" $cargo_file
         fi
+        echo "Updated version in $cargo_file"
+    else
+        echo "No Cargo.toml found in $(dirname $cargo_file)"
     fi
 }
+
 
 update_package_json() {
     local json_file="$1"
@@ -31,7 +39,7 @@ update_saito_wasm_version_in_js() {
 }
 
 
-members=("../saito-core" "../saito-wasm" "../saito-rust" "../saito-spammer" "../saito-js")
+members=("./saito-core" "./saito-wasm" "./saito-rust" "./saito-spammer" "./saito-js")
 
 for member in "${members[@]}"; do
     echo "Checking $member..."
