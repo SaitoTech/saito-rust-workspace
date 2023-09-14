daa76ee33a435e401160f02fd0b545403a830009
davik20 <onyejivic@gmail.com>
2023-09-14 08:35:29 +0100

    update script

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -3,26 +3,32 @@
 VERSION=$(cat ../VERSION | tr -d '\n')
 
 update_cargo() {
-    if [[ -f $1 ]]; then
-        sed -i "s/^version = .*/version = \"$VERSION\"/" $1
+    local cargo_file="$1"
+    if [[ -f $cargo_file ]]; then
+        OS=$(uname)
+        if [[ "$OS" == "Darwin" ]]; then 
+            sed -i "" "s/^version = .*/version = \"$VERSION\"/" $cargo_file
+        fi
     fi
 }
 
+
 update_package_json() {
-    if [[ -f $1 && $(jq .version $1) ]]; then
-        jq ".version = \"$VERSION\"" $1 | sponge $1
+    local json_file="$1"
+    if [[ -f $json_file ]]; then
+        if jq .version "$json_file" > /dev/null 2>&1; then
+            jq ".version = \"$VERSION\"" "$json_file" | sponge "$json_file"
+        fi
     fi
 }
 
 members=("../saito-core" "../saito-wasm" "../saito-rust" "../saito-spammer" "../saito-js")
 
 for member in "${members[@]}"; do
-    echo "Updating version in $member..."
+    echo "Checking $member..."
 
-    # Update cargo.toml
     update_cargo "$member/Cargo.toml"
     
-    # Update package.json
     update_package_json "$member/package.json"
 done
 
