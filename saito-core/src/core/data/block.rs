c34e4e62c1be80cdc582709f9d23ff4efd74e540
davik20 <onyejivic@gmail.com>
2023-09-29 13:45:21 +0100

    udpate debug

--- saito-core/src/core/data/block.rs
+++ saito-core/src/core/data/block.rs
@@ -1463,7 +1463,7 @@ impl Block {
     }
 
     pub fn generate_lite_block(&self, keylist: Vec<SaitoPublicKey>) -> Block {
-        dbg!(
+        debug!(
             "generating lite block for keys : {:?}",
             keylist.iter().map(hex::encode).collect::<Vec<String>>()
         );
