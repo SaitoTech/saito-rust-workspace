773c46a62249b83afca6e88a9bba2d2466f62b7f
davikstone <onyeji@saito.tech>
2023-11-29 00:32:02 +0300

    update package.json

--- saito-rust/src/saito/network_controller.rs
+++ saito-rust/src/saito/network_controller.rs
@@ -871,7 +871,10 @@ fn run_websocket_server(
                         error!("failed parsing buffer into a block");
                         return Err(warp::reject::not_found());
                     }
-                    let block = block.unwrap();
+                    let mut block = block.unwrap();
+
+                    block.generate();
+                    dbg!(&block.transactions.len());
                     let block = block.generate_lite_block(keylist);
                     let buffer = block.serialize_for_net(BlockType::Full);
                     let buffer_len = buffer.len();
