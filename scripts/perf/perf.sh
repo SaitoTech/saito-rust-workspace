4eacd243fe3e9b76692e41a03057ad5f412eda86
davikstone <onyeji@saito.tech>
2024-01-26 04:51:23 +0100

    update performance script

--- scripts/perf/perf.sh
+++ scripts/perf/perf.sh
@@ -1,12 +1,16 @@
-#!/bin/bash
 
 config_file="./scripts/perf/perf_config.json"
 
-main_node_dir=$(jq -r '.main_node_dir' "$config_file")
-spammer_node_dir=$(jq -r '.spammer_node_dir' "$config_file")
-verification_threads=$(jq -r '.verification_threads' "$config_file")
-burst_count=$(jq -r '.burst_count' "$config_file")
-tx_size=$(jq -r '.tx_size' "$config_file")
+get_config_value() {
+    jq -r ".$1" "$config_file"
+}
+
+main_node_dir=$(get_config_value 'main_node_dir')
+spammer_node_dir=$(get_config_value 'spammer_node_dir')
+verification_threads=$(get_config_value 'verification_threads')
+burst_count=$(get_config_value 'burst_count')
+tx_size=$(get_config_value 'tx_size')
+
 
 is_process_running() {
     pm2 show "$1" > /dev/null 2>&1
@@ -23,26 +27,37 @@ clear_blocks_directory() {
     fi
 }
 
+get_latest_block_name() {
+    ls -Art "$1/data/blocks" | tail -n 1
+}
+
+update_config_file() {
+    local config_path="$1"
+    local jq_script="$2"
+    jq "$jq_script" "$config_path" > temp.json && mv temp.json "$config_path"
+}
+
+start_pm2_service() {
+    local dir="$1"
+    local service_name="$2"
+    cd "$dir"
+    pm2 start "RUST_LOG=debug cargo run" --name "$service_name" --no-autorestart
+}
+
 clear_blocks_directory "$main_node_dir"
 clear_blocks_directory "$spammer_node_dir"
 
-main_node_config="$main_node_dir/configs/config.json"
-jq ".server.verification_threads = $verification_threads" "$main_node_config" > temp.json && mv temp.json "$main_node_config"
+update_config_file "$main_node_dir/configs/config.json" ".server.verification_threads = $verification_threads"
+update_config_file "$spammer_node_dir/configs/config.json" ".spammer.burst_count = $burst_count | .spammer.tx_size = $tx_size"
 
-spammer_node_config="$spammer_node_dir/configs/config.json"
-jq ".spammer.burst_count = $burst_count | .spammer.tx_size = $tx_size" "$spammer_node_config" > temp.json && mv temp.json "$spammer_node_config"
-
-cd "$main_node_dir"
-pm2 start "RUST_LOG=debug cargo run" --name "main_node" --no-autorestart
-until is_process_running "main_node"; do
-    sleep 1
-done
+start_pm2_service "$main_node_dir" "main_node"
+until is_process_running "main_node"; do sleep 1; done
 echo "Main node is running."
 
-cd "$spammer_node_dir"
-pm2 start "RUST_LOG=debug cargo run" --name "spammer_node" --no-autorestart
+start_pm2_service "$spammer_node_dir" "spammer_node"
 echo "Spammer node is started."
 
+# Main monitoring loop
 while true; do
     stats_file="$main_node_dir/data/saito.stats"
     output_csv="$main_node_dir/perf_result.csv"
@@ -91,15 +106,13 @@ while true; do
                 # Check RAM usage after loading blocks
                 ram_after_loading_blocks=$(vm_stat | awk -F': ' '/Pages (active|inactive|wired down)/ {used+=$2} END {print used * 4096 / 1048576 " MB"}')
 
-
                 # Wait for 'starting websocket server' to measure initial RAM usage
                 while ! grep -m1 "starting websocket server" "$output_file" > /dev/null; do
                     sleep 1
                 done
                 ram_after_initial_run=$(vm_stat | awk -F': ' '/Pages (active|inactive|wired down)/ {used+=$2} END {print used * 4096 / 1048576 " MB"}')
 
-
-                #  check if main node has properly started
+                # check if main node has properly started
                 until is_process_running "main_node"; do
                 sleep 1
                 done
@@ -111,11 +124,25 @@ while true; do
                 echo "Starting spammer node and monitoring for fetching blocks."
                 pm2 start "spammer_node"
                 fetch_start_time=$(date +%s)
-                
-                # Wait for the specific phrase in spammer_node logs
-                while ! grep -m1 "adding fetched block to mempool" "$output_file_spammer_node" > /dev/null; do
-                sleep 1 
+
+                latest_block_name=$(get_latest_block_name "$main_node_dir")
+                if [ -z "$latest_block_name" ]; then
+                    echo "No block files found in $spammer_node_dir/data/blocks"
+                    exit 1
+                fi
+
+                block_identifier=$(echo "$latest_block_name" | grep -oE '[^-]*\.sai$' | sed 's/\.sai$//')
+                if [ -z "$block_identifier" ]; then
+                    echo "Unable to extract block identifier from $latest_block_name"
+                    exit 1
+                fi
+                search_phrase="fetching block : .*\/$block_identifier"
+                echo "Latest block name: $latest_block_name"
+                echo "Searching for phrase: $search_phrase"
+                while ! grep -m1 "$search_phrase" "$output_file_spammer_node" > /dev/null; do
+                    sleep 1 
                 done
+
                 fetch_end_time=$(date +%s)
                 time_to_fetch_blocks=$((fetch_end_time - fetch_start_time))
                 ram_after_fetching_blocks=$(vm_stat | awk -F': ' '/Pages (active|inactive|wired down)/ {used+=$2} END {print used * 4096 / 1048576 " MB"}')
@@ -127,7 +154,7 @@ while true; do
                 echo "Time to fetch blocks : $time_to_fetch_blocks"
                 echo "RAM usage after fetching blocks blocks: $ram_after_fetching_blocks"
 
-                pm2 kill 
+                # pm2 kill 
 
 
 
