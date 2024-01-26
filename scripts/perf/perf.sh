9da487e87a4b001a6dd9b486eb020cd846776c27
davikstone <onyeji@saito.tech>
2024-01-26 03:57:45 +0100

    update performance script

--- scripts/perf/perf.sh
+++ scripts/perf/perf.sh
@@ -47,6 +47,8 @@ while true; do
     stats_file="$main_node_dir/data/saito.stats"
     output_csv="$main_node_dir/perf_result.csv"
 
+
+    
     STATUS=$(pm2 show spammer_node | grep "status" | awk '{print $4}')
     echo "$STATUS"
 
@@ -68,42 +70,76 @@ while true; do
         average_block_size=$(echo "$total_block_size $num_block_files" | awk '{print int($1/$2)}')
 
 
-        
-        echo "Restarting main node and monitoring for block loading."
-        pm2 start "main_node"
-        start_time=$(date +%s)
+                 # Restart main node
+                echo "Restarting main node and monitoring for block loading."
+                pm2 start "main_node"
+                start_time=$(date +%s)
+
+                output_file="$main_node_dir/main_node_output.log"
+                pm2 logs main_node > "$output_file" 2>&1 &
+
+                output_file_spammer_node="$spammer_node_dir/spammer_node_output.log"
+               pm2 logs spammer_node > "$output_file_spammer_node" 2>&1 &  
+
+                # Time to load blocks
+                while ! grep -m1 "0 blocks remaining to be loaded" "$output_file" > /dev/null; do
+                    sleep 1 
+                done
+                end_time=$(date +%s)
+                time_to_load_blocks=$((end_time - start_time))
+
+                # Check RAM usage after loading blocks
+                ram_after_loading_blocks=$(vm_stat | awk -F': ' '/Pages (active|inactive|wired down)/ {used+=$2} END {print used * 4096 / 1048576 " MB"}')
+
+
+                # Wait for 'starting websocket server' to measure initial RAM usage
+                while ! grep -m1 "starting websocket server" "$output_file" > /dev/null; do
+                    sleep 1
+                done
+                ram_after_initial_run=$(vm_stat | awk -F': ' '/Pages (active|inactive|wired down)/ {used+=$2} END {print used * 4096 / 1048576 " MB"}')
+
+
+                #  check if main node has properly started
+                until is_process_running "main_node"; do
+                sleep 1
+                done
+
+                # clear blocks in spammer node directory
+                clear_blocks_directory "$spammer_node_dir"
+
+                # Start spammer node and monitor for fetching blocks
+                echo "Starting spammer node and monitoring for fetching blocks."
+                pm2 start "spammer_node"
+                fetch_start_time=$(date +%s)
+                
+                # Wait for the specific phrase in spammer_node logs
+                while ! grep -m1 "adding fetched block to mempool" "$output_file_spammer_node" > /dev/null; do
+                sleep 1 
+                done
+                fetch_end_time=$(date +%s)
+                time_to_fetch_blocks=$((fetch_end_time - fetch_start_time))
+                ram_after_fetching_blocks=$(vm_stat | awk -F': ' '/Pages (active|inactive|wired down)/ {used+=$2} END {print used * 4096 / 1048576 " MB"}')
 
 
 
-        output_file="$main_node_dir/main_node_output.log"
-        pm2 logs main_node > "$output_file" 2>&1 &
+                echo "All blocks loaded. Time taken: $time_to_load_blocks seconds."
+                echo "RAM usage before initial run: $ram_after_initial_run"
+                echo "Time to fetch blocks : $time_to_fetch_blocks"
+                echo "RAM usage after fetching blocks blocks: $ram_after_fetching_blocks"
 
-   
-        while ! grep -m1 "0 blocks remaining to be loaded" "$output_file" > /dev/null; do
-            sleep 5 
-        done
-        end_time=$(date +%s)
-        time_to_load_blocks=$((end_time - start_time))
-
-        time_to_fetch_blocks="N/A"
-        ram_after_initial_run="N/A"
-        ram_after_loading_blocks="N/A"
-        ram_after_fetching_blocks="N/A"
+                pm2 kill 
 
 
 
 
 
+                echo "tx_rate_from_spammer,tx_size,verification_thread_count,max_tx_rate_at_network_thread,max_tx_rate_at_verification_threads,total_txs,block_count,longest_chain_length,total_block_size,average_block_size,time_to_load_blocks,time_to_fetch_blocks,ram_after_initial_run,ram_after_loading_blocks,ram_after_fetching_blocks" > "$output_csv"
+
+                # Write the data in snake_case format to the CSV file
+                echo "$burst_count,$tx_size,$verification_threads,$max_tx_rate_network_thread,$max_tx_rate_verification_threads,$total_txs,$block_count,$longest_chain_length,$total_block_size,$average_block_size,$time_to_load_blocks,$time_to_fetch_blocks,$ram_after_initial_run,$ram_after_loading_blocks,$ram_after_fetching_blocks" >> "$output_csv"
 
 
-
-echo "tx_rate_from_spammer,tx_size,verification_thread_count,max_tx_rate_at_network_thread,max_tx_rate_at_verification_threads,total_txs,block_count,longest_chain_length,total_block_size,average_block_size,time_to_load_blocks,time_to_fetch_blocks,ram_after_initial_run,ram_after_loading_blocks,ram_after_fetching_blocks" > "$output_csv"
-
-# Write the data in snake_case format to the CSV file
-echo "$burst_count,$tx_size,$verification_threads,$max_tx_rate_network_thread,$max_tx_rate_verification_threads,$total_txs,$block_count,$longest_chain_length,$total_block_size,$average_block_size,$time_to_load_blocks,$time_to_fetch_blocks,$ram_after_initial_run,$ram_after_loading_blocks,$ram_after_fetching_blocks" >> "$output_csv"
-
-
-        echo "CSV file created at $output_csv"
+                echo "CSV file created at $output_csv"
 
         break
     fi
