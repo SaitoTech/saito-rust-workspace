a96d510bea47741566a998a55a2deb464234f17f
davikstone <onyeji@saito.tech>
2024-02-02 01:07:16 +0100

    update

--- scripts/perf/perf.sh
+++ scripts/perf/perf.sh
@@ -1,7 +1,10 @@
+
 #!/bin/bash
 
 SCRIPT_DIR=$(dirname "$0")
+
 cd "$SCRIPT_DIR"
+
 config_file="./perf_config.json"
 
 get_config_value() {
@@ -12,7 +15,10 @@ main_node_dir=$(get_config_value 'main_node_dir')
 spammer_node_dir=$(get_config_value 'spammer_node_dir')
 output_csv="./perf_result.csv"
 
+
 echo "tx_rate_from_spammer,tx_payload_size,verification_thread_count,max_tx_rate_at_network_thread,max_tx_rate_at_verification_threads,total_txs,block_count,longest_chain_length,total_block_size,average_block_size,transaction_throughput,data_throughput" > "$output_csv"
+
+
 
 is_process_running() {
     pm2 show "$1" > /dev/null 2>&1
@@ -23,6 +29,7 @@ is_process_running() {
 command_exists() {
     command -v "$@" > /dev/null 2>&1
 }
+
 
 install_pm2() {
     echo "Checking for PM2 installation..."
@@ -47,13 +54,14 @@ install_pm2() {
     fi
 }
 
+
 get_ram_usage() {
     os_name=$(uname -s)
     case "$os_name" in
-        Darwin)
+        Darwin) # 
             echo $(vm_stat | awk -F': ' '/Pages (active|inactive|wired down)/ {used+=$2} END {print used * 4096 / 1048576 " MB"}')
             ;;
-        Linux)
+        Linux) 
             echo $(free -m | awk '/Mem:/ {print $3 " MB"}')
             ;;
         *)
@@ -63,6 +71,7 @@ get_ram_usage() {
 }
 
 clear_blocks_directory() {
+    # echo "$1"
     dir_path="$1/data/blocks"
     if [ -d "$dir_path" ]; then
         rm -rf "$dir_path"/*
@@ -91,6 +100,8 @@ start_pm2_service() {
         cd "$dir" || exit
         pm2 start "RUST_LOG=debug cargo run" --name "$service_name" --no-autorestart
     )
+    
+    # echo "Back to script directory: $SCRIPT_DIR"
 }
 
 create_or_append_issuance() {
@@ -105,6 +116,7 @@ create_or_append_issuance() {
         echo "100000	v4S4wiwJQVP3fNZkpYLG59PFNMXmHnNNpE4kREfCeJic	Normal" > "$file"
     fi
 }
+
 
 loader() {
     local i=0
@@ -133,7 +145,6 @@ loader() {
 create_or_append_issuance "$main_node_dir"
 install_pm2
 test_configs=$(jq -c '.perf_tests[]' "$config_file")
-
 for config in $test_configs; do
     echo "Running test with configuration: $config"
 
@@ -148,49 +159,152 @@ for config in $test_configs; do
     update_config_file "$spammer_node_dir/configs/config.json" ".spammer.burst_count = $burst_count | .spammer.tx_size = $tx_payload_size"
 
     start_pm2_service "$main_node_dir" "main_node"
-
-    until is_process_running "main_node"; do
-        sleep 1
-    done
+    until is_process_running "main_node"; do sleep 1; done
     echo "Main node is running."
+
+
 
     start_pm2_service "$spammer_node_dir" "spammer_node"
     echo "Spammer node is started."
 
+
     while true; do
-        stats_file="$main_node_dir/data/saito.stats"
-        output_csv="./perf_result.csv"
+    stats_file="$main_node_dir/data/saito.stats"
+    output_csv="./perf_result.csv"
 
-        echo "Waiting for 5 minutes to gather data..."
-        sleep 10  # Sleep for 300 seconds (5 minutes)
 
+
+    # loader
+
+    # Wait for 5 minutes
+    echo "Waiting for 5 minutes to gather data..."
+    sleep 10  # Sleep for 300 seconds (5 minutes)
+
+    
+
+    # STATUS=$(pm2 show spammer_node | grep "status" | awk '{print $4}')
+
+    # if [ "$STATUS" != "online" ]; then
         echo "Spammer process has stopped."
         echo "Stopping main node and spammer node"
         pm2 stop spammer_node
         pm2 stop main_node
+
 
         max_tx_rate_network_thread=$(grep "network::incoming_msgs" "$stats_file" | awk '{print $11}' | tr -d ',' | sort -nr | head -n 1)
         max_tx_rate_verification_threads=$(grep "verification_.*::processed_txs" "$stats_file" | awk '{print $11}' | tr -d ',' | sort -nr | head -n 1)
         total_txs=$(grep "routing::incoming_msgs" "$stats_file" | awk '{print $5}' | tr -d ',' | sort -nr | head -n 1)
         block_count=$(grep "blockchain::state" "$stats_file" | awk '{print $8}' | tr -d ',' | sort -nr | head -n 1)
         longest_chain_length=$(grep "blockchain::state" "$stats_file" | awk '{print $11}' | tr -d ',' | sort -nr | head -n 1)
-        txs_in_blocks=$(grep "blockchain::state" "$stats_file" | awk '{print $17}' | tr -d ',' | tr -d '\r' |  sort -nr | head -n 1)
+        txs_in_blocks=$(grep "blockchain::state" "$stats_file" | awk '{print $17}' | tr -d ',' | tr -d '\r' | sort -nr | head -n 1)
+
+        echo $txs_in_blocks
+
 
         total_block_size=$(du -ck "$main_node_dir/data/blocks/"* | grep "total" | awk '{print $1}')
         num_block_files=$(find "$main_node_dir/data/blocks/" -type f | wc -l)
         average_block_size=$(echo "$total_block_size $num_block_files" | awk '{print int($1/$2)}')
 
-        transaction_throughput=$(echo "scale=2; $txs_in_blocks / 300" | bc)
-        data_throughput=$(echo "scale=2; $tx_payload_size * $txs_in_blocks / 300" | bc)
+            # Calculate Transaction and Data Throughput
+            transaction_throughput=$(echo "scale=2; $txs_in_blocks / 300" | bc)
+data_throughput=$(echo "scale=2; $tx_payload_size * $txs_in_blocks / 300" | bc)
 
-        echo "Transaction Throughput (tx/s): $transaction_throughput"
-        echo "Data Throughput (bytes/s): $data_throughput"
 
-        echo "$burst_count,$tx_payload_size,$verification_threads,$max_tx_rate_network_thread,$max_tx_rate_verification_threads,$total_txs,$block_count,$longest_chain_length,$total_block_size,$average_block_size,$transaction_throughput,$data_throughput" >> "$output_csv"
+            echo "Transaction Throughput (tx/s): $transaction_throughput"
+            echo "Data Throughput (bytes/s): $data_throughput"
+
+            # Append to CSV
+            echo "$burst_count,$tx_payload_size,$verification_threads,$max_tx_rate_network_thread,$max_tx_rate_verification_threads,$total_txs,$block_count,$longest_chain_length,$total_block_size,$average_block_size,$transaction_throughput,$data_throughput" >> "$output_csv"
+
+
+            # echo "$burst_count,$tx_payload_size,$verification_threads,$max_tx_rate_network_thread,$max_tx_rate_verification_threads,$total_txs,$block_count,$longest_chain_length,$total_block_size,$average_block_size" >> "$output_csv"
+
+                 # Restart main node
+                # echo "Restarting main node and monitoring for block loading."
+            #     pm2 restart "main_node"
+            #     start_time=$(date +%s)
+
+            #     output_file="$main_node_dir/main_node_output.log"
+            #     pm2 logs main_node > "$output_file" 2>&1 &
+
+            #     output_file_spammer_node="$spammer_node_dir/spammer_node_output.log"
+            #    pm2 logs spammer_node > "$output_file_spammer_node" 2>&1 &  
+
+            #     # Time taken to load blocks
+            #     while ! grep -m1 "0 blocks remaining to be loaded" "$output_file" > /dev/null; do
+            #         sleep 1 
+            #     done
+            #     end_time=$(date +%s)
+            #     time_to_load_blocks=$((end_time - start_time))
+
+            #     # Check RAM usage after loading blocks
+            #     ram_after_loading_blocks=$(get_ram_usage)
+
+            #     # Wait for 'starting websocket server' to measure initial RAM usage
+            #     while ! grep -m1 "starting websocket server" "$output_file" > /dev/null; do
+            #         sleep 1
+            #     done
+            #     ram_after_initial_run=$(get_ram_usage)
+
+            #     # check if main node has properly started
+            #     until is_process_running "main_node"; do
+            #     sleep 1
+            #     done
+
+            #     # clear blocks in spammer node directory
+            #     clear_blocks_directory "$spammer_node_dir"
+
+            #     # Start spammer node and monitor for fetching blocks
+            #     echo "Starting spammer node and monitoring for fetching blocks."
+            #     pm2 start "spammer_node"
+            #     fetch_start_time=$(date +%s)
+
+            #     latest_block_name=$(get_latest_block_name "$main_node_dir")
+            #     if [ -z "$latest_block_name" ]; then
+            #         echo "No block files found in $spammer_node_dir/data/blocks"
+            #         exit 1
+            #     fi
+
+            #     block_identifier=$(echo "$latest_block_name" | grep -oE '[^-]*\.sai$' | sed 's/\.sai$//')
+            #     if [ -z "$block_identifier" ]; then
+            #         echo "Unable to extract block identifier from $latest_block_name"
+            #         exit 1
+            #     fi
+            #     search_phrase="fetching block : .*\/$block_identifier"
+            #     echo "Latest block id: $latest_block_name"
+            #     while ! grep -m1 "$search_phrase" "$output_file_spammer_node" > /dev/null; do
+            #         sleep 1 
+            #     done
+
+            #     fetch_end_time=$(date +%s)
+            #     time_to_fetch_blocks=$((fetch_end_time - fetch_start_time))
+            #     ram_after_fetching_blocks=$(get_ram_usage)
+
+
+
+            #     echo "All blocks loaded. Time taken: $time_to_load_blocks seconds."
+            #     echo "RAM usage before initial run: $ram_after_initial_run"
+            #     echo "Time to fetch blocks : $time_to_fetch_blocks"
+            #     echo "RAM usage after fetching blocks blocks: $ram_after_fetching_blocks"
+
+
+
+
+            #     echo "$burst_count,$tx_payload_size,$verification_threads,$max_tx_rate_network_thread,$max_tx_rate_verification_threads,$total_txs,$block_count,$longest_chain_length,$total_block_size,$average_block_size,$time_to_load_blocks,$time_to_fetch_blocks,$ram_after_initial_run,$ram_after_loading_blocks,$ram_after_fetching_blocks" >> "$output_csv"
+
+
+            #     echo "CSV file created at $output_csv"
+
+            #     pm2 "kill"
 
         break
-        sleep 1
-    done
+    # fi
+    sleep 1
+done
+
 
     pm2 list
 done
+
+
+
