f83dc04501fe92cd6ca3f58cebff19133332cbf4
davikstone <onyeji@saito.tech>
2024-02-06 04:43:57 +0100

    update readme

---
+++ scripts/bootstrap_mac.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Script to setup the basic requirements for running a saito rust node on macOS
+
+if ! command -v brew &> /dev/null
+then
+    echo "Homebrew not found. Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+brew update
+
+brew install llvm clang pkg-config node npm python3
+
+
+cargo install wasm-pack
+
+
+cargo build
