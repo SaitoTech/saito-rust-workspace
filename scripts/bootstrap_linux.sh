08b9434d0c7a45763867f526f1295adff4bb018c
davikstone <onyeji@saito.tech>
2024-02-06 05:42:49 +0100

    update readme

--- scripts/bootstrap_linux.sh
+++ scripts/bootstrap_linux.sh
@@ -3,9 +3,10 @@
 # Script to setup the basic requirements for running a saito rust node
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 sudo apt update
-sudo NEEDRESTART_MODE=a apt install -y build-essential libssl-dev pkg-config nodejs npm clang gcc-multilib python-is-python3
+sudo NEEDRESTART_MODE=a apt install -y build-essential libssl-dev pkg-config nodejs npm clang gcc-multilib python-is-python3 cargo
 tset
 #cargo install flamegraph
+sudo npm install -g pm2
 cargo install wasm-pack
 cargo build
 
--- scripts/bootstrap_mac.sh
+++ scripts/bootstrap_mac.sh
@@ -13,10 +13,8 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 
 brew update
 
-brew install llvm clang pkg-config node npm python3
-
+brew install llvm clang pkg-config node npm python3 cargo
 
 cargo install wasm-pack
-
 
 cargo build
