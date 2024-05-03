1c587764f325ebdb3f18b0248382d2da07f7f3f8
davikstone <onyeji@saito.tech>
2024-05-03 14:37:38 +0100

    update peer

--- saito-core/src/core/io/network.rs
+++ saito-core/src/core/io/network.rs
@@ -471,6 +471,14 @@ impl Network {
             let peers = self.peer_lock.read().await;
 
             if let Some(peer) = peers.index_to_peers.get(&peer_index) {
+                let result = peer
+                    .compare_versions(block_hash, self.wallet_lock.clone())
+                    .await;
+
+                if result.is_none() {
+                    return None;
+                }
+
                 if peer.block_fetch_url.is_empty() {
                     debug!(
                         "won't fetch block : {:?} from peer : {:?} since no url found",
