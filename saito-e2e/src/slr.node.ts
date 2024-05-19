0285e431a8ee2104723acf6c480c3faf7f1c75c5
Sanka Darshana <sankadarshana@gmail.com>
2024-05-20 02:03:22 +0530

    add new tests #750

---
+++ saito-e2e/src/slr.node.ts
@@ -0,0 +1,13 @@
+import { type Page, type Locator } from "@playwright/test";
+
+export default class SlrNode {
+  constructor() {}
+
+  async startNode() {}
+
+  async stopNode() {}
+
+  async clearDataFolder() {}
+
+  async setIssuance(issuance: string[]) {}
+}
