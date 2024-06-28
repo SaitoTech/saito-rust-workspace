2292ca1e36d344d6fa8a90216dcdc8b343629b0c
sankadarshana <sankadarshana@gmail.com>
2024-06-28 15:43:19 +0530

    version upgrade

--- saito-js/lib/wallet.ts
+++ saito-js/lib/wallet.ts
@@ -1,120 +1,125 @@
-import type { WasmWallet } from "saito-wasm/pkg/node/index";
-import { WasmWalletSlip } from "saito-wasm/pkg/node/index";
+import type {WasmWallet} from "saito-wasm/pkg/node/index";
+import {WasmWalletSlip} from "saito-wasm/pkg/node/index";
 import Saito from "../saito";
 import WasmWrapper from "./wasm_wrapper";
-import { toBase58 } from "./util";
+import {toBase58} from "./util";
+import Transaction from "./transaction";
 
 export const DefaultEmptyPrivateKey =
-  "0000000000000000000000000000000000000000000000000000000000000000";
+    "0000000000000000000000000000000000000000000000000000000000000000";
 
 export const DefaultEmptyPublicKey =
-  toBase58("000000000000000000000000000000000000000000000000000000000000000000");
+    toBase58("000000000000000000000000000000000000000000000000000000000000000000");
 
 export const DefaultEmptyBlockHash =
-  "0000000000000000000000000000000000000000000000000000000000000000";
+    "0000000000000000000000000000000000000000000000000000000000000000";
 
 export default class Wallet extends WasmWrapper<WasmWallet> {
-  public static Type: any;
+    public static Type: any;
 
-  constructor(wallet: WasmWallet) {
-    super(wallet);
-  }
-
-  public async save() {
-    return this.instance.save();
-  }
-
-  public async load() {
-    return this.instance.load();
-  }
-
-  public async reset() {
-    return this.instance.reset();
-  }
-
-  public async getPublicKey() {
-    let key = await this.instance.get_public_key();
-    return key === DefaultEmptyPublicKey ? "" : key;
-  }
-
-  public async setPublicKey(key: string) {
-    if (key === "") {
-      key = DefaultEmptyPublicKey;
+    constructor(wallet: WasmWallet) {
+        super(wallet);
     }
-    return this.instance.set_public_key(key);
-  }
 
-  public async getPrivateKey() {
-    let key = await this.instance.get_private_key();
-    return key === DefaultEmptyPrivateKey ? "" : key;
-  }
-
-  public async setPrivateKey(key: string) {
-    if (key === "") {
-      key = DefaultEmptyPrivateKey;
+    public async save() {
+        return this.instance.save();
     }
-    return this.instance.set_private_key(key);
-  }
 
-  public async getBalance() {
-    return this.instance.get_balance();
-  }
-
-  public async getPendingTxs() {
-    let txs = await this.instance.get_pending_txs();
-    return txs.map((tx: any) => Saito.getInstance().factory.createTransaction(tx));
-  }
-
-  public async getSlips(): Promise<WalletSlip[]> {
-    return this.instance.get_slips().then(slips => {
-      return slips.map(slip => new WalletSlip(slip));
-    });
-  }
-
-  public async addSlips(slips: WalletSlip[]) {
-    for (const slip of slips) {
-      await this.instance.add_slip(slip.instance);
+    public async load() {
+        return this.instance.load();
     }
-  }
 
-  public async getKeyList(): Promise<string[]> {
-    return this.instance.get_key_list();
-  }
+    public async reset() {
+        return this.instance.reset();
+    }
 
-  public async setKeyList(keylist: string[]) {
-    await this.instance.set_key_list(keylist);
-  }
+    public async getPublicKey() {
+        let key = await this.instance.get_public_key();
+        return key === DefaultEmptyPublicKey ? "" : key;
+    }
+
+    public async setPublicKey(key: string) {
+        if (key === "") {
+            key = DefaultEmptyPublicKey;
+        }
+        return this.instance.set_public_key(key);
+    }
+
+    public async getPrivateKey() {
+        let key = await this.instance.get_private_key();
+        return key === DefaultEmptyPrivateKey ? "" : key;
+    }
+
+    public async setPrivateKey(key: string) {
+        if (key === "") {
+            key = DefaultEmptyPrivateKey;
+        }
+        return this.instance.set_private_key(key);
+    }
+
+    public async getBalance() {
+        return this.instance.get_balance();
+    }
+
+    public async getPendingTxs() {
+        let txs = await this.instance.get_pending_txs();
+        return txs.map((tx: any) => Saito.getInstance().factory.createTransaction(tx));
+    }
+
+    public async getSlips(): Promise<WalletSlip[]> {
+        return this.instance.get_slips().then(slips => {
+            return slips.map(slip => new WalletSlip(slip));
+        });
+    }
+
+    public async addSlips(slips: WalletSlip[]) {
+        for (const slip of slips) {
+            await this.instance.add_slip(slip.instance);
+        }
+    }
+
+    public async getKeyList(): Promise<string[]> {
+        return this.instance.get_key_list();
+    }
+
+    public async setKeyList(keylist: string[]) {
+        await this.instance.set_key_list(keylist);
+    }
+
+    public async addToPending(tx: Transaction) {
+        return this.instance.add_to_pending(tx.instance);
+    }
 }
 
 export class WalletSlip extends WasmWrapper<WasmWalletSlip> {
-  public static Type: any;
+    public static Type: any;
 
-  public constructor(slip?: WasmWalletSlip) {
-    if (!slip) {
-      slip = new WalletSlip.Type();
+    public constructor(slip?: WasmWalletSlip) {
+        if (!slip) {
+            slip = new WalletSlip.Type();
+        }
+        super(slip!);
     }
-    super(slip!);
-  }
 
-  public toJson() {
-    return {
-      utxokey: this.instance.get_utxokey(),
-      lc: this.instance.is_lc(),
-      spent: this.instance.is_spent(),
-      blockId: this.instance.get_block_id(),
-      txIndex: this.instance.get_tx_ordinal(),
-      slipIndex: this.instance.get_slip_index(),
-      amount: this.instance.get_amount()
-    };
-  }
+    public toJson() {
+        return {
+            utxokey: this.instance.get_utxokey(),
+            lc: this.instance.is_lc(),
+            spent: this.instance.is_spent(),
+            blockId: this.instance.get_block_id(),
+            txIndex: this.instance.get_tx_ordinal(),
+            slipIndex: this.instance.get_slip_index(),
+            amount: this.instance.get_amount()
+        };
+    }
 
-  public copyFrom(json: any) {
-    this.instance.set_utxokey(json.utxokey);
-    this.instance.set_lc(json.lc);
-    this.instance.set_spent(json.spent);
-    this.instance.set_block_id(BigInt(json.blockId));
-    this.instance.set_tx_ordinal(BigInt(json.txIndex));
-    this.instance.set_slip_index(json.slipIndex);
-    this.instance.set_amount(BigInt(json.amount));
-  }
+    public copyFrom(json: any) {
+        this.instance.set_utxokey(json.utxokey);
+        this.instance.set_lc(json.lc);
+        this.instance.set_spent(json.spent);
+        this.instance.set_block_id(BigInt(json.blockId));
+        this.instance.set_tx_ordinal(BigInt(json.txIndex));
+        this.instance.set_slip_index(json.slipIndex);
+        this.instance.set_amount(BigInt(json.amount));
+    }
 }
