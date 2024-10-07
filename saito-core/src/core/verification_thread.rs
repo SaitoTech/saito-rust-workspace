be98992adcbdd579c956a2f6d6dbcab3ff4f4628
sankadarshana <sankadarshana@gmail.com>
2024-10-08 04:03:21 +0530

    Write peer state and stats to a human readable file for analytical purposes #828

--- saito-core/src/core/verification_thread.rs
+++ saito-core/src/core/verification_thread.rs
@@ -139,7 +139,7 @@ impl VerificationThread {
             );
             let mut peers = self.peer_lock.write().await;
             if let Some(peer) = peers.find_peer_by_index_mut(peer_index) {
-                // NOTE : this means if we cannot deserialize a block from the buffer we mark it as blacklisted.
+                // NOTE : this means if we receive an invalid block, peer is blacklisted.
                 peer.invalid_block_limiter.increase();
             }
             return;
