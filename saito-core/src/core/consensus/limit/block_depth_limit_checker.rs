b83e42d9cee88ed5a2511dd8054ef671b7f2ca9f
sankadarshana <sankadarshana@gmail.com>
2024-10-07 07:04:58 +0530

    function definitions for checking block depth limits

--- saito-core/src/core/consensus/limit/block_depth_limit_checker.rs
+++ saito-core/src/core/consensus/limit/block_depth_limit_checker.rs
@@ -1,24 +1,39 @@
-use crate::core::defs::{BlockId, Timestamp};
+use crate::core::consensus::block::Block;
+use crate::core::consensus::blockchain::Blockchain;
+use crate::core::defs::{BlockHash, BlockId, ForkId, Timestamp};
+use ahash::HashMap;
 use std::time::Duration;
 
-#[derive(Default, Debug, Copy, Clone)]
+pub const FORK_BLOCK_DEPTH_LIMIT: BlockId = 1_000;
+
+pub(crate) enum BlockDepthCheckResult {
+    FetchBlock(BlockId, BlockHash),
+    ProceedNormally(Block),
+}
+
+#[derive(Default, Debug, Clone)]
 pub struct BlockDepthLimitChecker {
     count: u64,
     window: Timestamp,
+    block_checks: HashMap<BlockId, Block>,
 }
 
 impl BlockDepthLimitChecker {
-    pub fn increase_block_count_at_depth(&mut self, block_depth: BlockId) {
-        todo!()
-    }
-    pub fn has_limit_exceeded(&mut self, current_time: Timestamp) -> bool {
-        false
-    }
-
     pub fn builder(count: u64, duration: Duration) -> Self {
         Self {
             count,
             window: duration.as_millis() as Timestamp,
+            block_checks: Default::default(),
         }
+    }
+    pub fn should_fetch_fork_first(
+        &mut self,
+        block: Block,
+        blockchain: &Blockchain,
+    ) -> BlockDepthCheckResult {
+        if block.id > blockchain.get_latest_block_id() + FORK_BLOCK_DEPTH_LIMIT {
+            return BlockDepthCheckResult::FetchBlock(block.id, block.hash);
+        }
+        todo!()
     }
 }
