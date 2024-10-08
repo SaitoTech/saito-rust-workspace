a04e4767582d72d898d9368e61355381a7bf4efa
sankadarshana <sankadarshana@gmail.com>
2024-10-08 10:54:20 +0530

    Write peer state and stats to a human readable file for analytical purposes #828

--- saito-core/src/core/consensus/peers/peer_state_writer.rs
+++ saito-core/src/core/consensus/peers/peer_state_writer.rs
@@ -1,5 +1,6 @@
-use crate::core::defs::{PrintForLog, SaitoPublicKey, Timestamp};
+use crate::core::defs::{PeerIndex, PrintForLog, SaitoPublicKey, Timestamp};
 use crate::core::io::interface_io::InterfaceIO;
+use crate::core::routing_thread::PeerState;
 use ahash::HashMap;
 use std::io::Error;
 use std::time::Duration;
@@ -9,13 +10,29 @@ const PEER_STATE_FILENAME: &str = "./data/peer_state.txt";
 pub(crate) const PEER_STATE_WRITE_PERIOD: Timestamp =
     Duration::from_secs(1).as_millis() as Timestamp;
 
-#[derive(Default, Debug, Clone)]
+#[derive(Debug, Clone)]
 pub(crate) struct PeerStateEntry {
+    pub peer_index: PeerIndex,
+    pub public_key: SaitoPublicKey,
     pub msg_limit_exceeded: bool,
     pub invalid_blocks_received: bool,
     pub same_depth_blocks_received: bool,
     pub too_far_blocks_received: bool,
     pub limited_till: Option<Timestamp>,
+}
+
+impl Default for PeerStateEntry {
+    fn default() -> Self {
+        Self {
+            peer_index: 0,
+            public_key: [0; 33],
+            msg_limit_exceeded: false,
+            invalid_blocks_received: false,
+            same_depth_blocks_received: false,
+            too_far_blocks_received: false,
+            limited_till: None,
+        }
+    }
 }
 
 #[derive(Clone, Debug, Default)]
@@ -37,20 +54,21 @@ impl PeerStateWriter {
     /// ```
     pub(crate) async fn write_state(
         &self,
-        data: HashMap<SaitoPublicKey, PeerStateEntry>,
+        data: Vec<PeerStateEntry>,
         io_handler: &mut Box<dyn InterfaceIO + Send + Sync>,
     ) -> Result<(), Error> {
         let line =
-            "key,limited_till,msg_limit,invalid_blocks_limit,same_depth_limit,too_far_block_limit\r\n"
+            "peer_index,public_key,limited_till,msg_limit,invalid_blocks_limit,same_depth_limit,too_far_block_limit\r\n"
                 .to_string();
         io_handler
             .write_value(PEER_STATE_FILENAME, line.as_bytes())
             .await?;
 
-        for (public_key, data) in data.iter() {
+        for data in data.iter() {
             let line = format!(
-                "{:?},{:?},{:?},{:?},{:?},{:?}\r\n",
-                public_key.to_base58(),
+                "{:?},{:?},{:?},{:?},{:?},{:?},{:?}\r\n",
+                data.peer_index,
+                data.public_key.to_base58(),
                 data.limited_till.unwrap_or(0),
                 data.msg_limit_exceeded,
                 data.invalid_blocks_received,
